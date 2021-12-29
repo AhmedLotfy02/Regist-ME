c678d3e1afbc1e47d0970f8203bb6305e64d1189
Ahmedsabry11 <75908511+Ahmedsabry11@users.noreply.github.com>
2021-12-29 23:38:26 +0200

    Update set_forbidden.asm

--- source/set_forbidden.asm
+++ source/set_forbidden.asm
@@ -1,30 +1,54 @@
-.model small
-.Data
+.model large 
+.stack 64
+.Data 
+; general variables ----------------------------------------------------------------------------------------------
 ; All instructions, digits and registers
-instructions db "mov ", "add ", "sub ", "mul ", "div ", "and ", "or ", "nor ", "xor ", "shr ", "shl ", "ror ", "rol ", "push ", "pop ", "inc ", "dec "  
-registers db "ax ", "al ", "ah ", "bx ", "bl ", "bh ", "cx ", "cl ", "ch ", "dx ", "dl ", "dh ", "si ", "di ", "sp ", "bp ","val "
-Player1_Data_Register dw '0000','0000','0000','0000','0000','0000','0000','0000','0000'
-;------------------------- Ax-----BX-----CX-----DX-----SI-----DI-----SP-----BP----value-----  
-Player2_Data_Register dw '0000','0000','0000','0000','0000','0000','0000','0000','0000'
-;------------------------- Ax-----BX-----CX-----DX-----SI-----DI-----SP-----BP----value-----
-digits db "0 ", "1 ", "2 ", "3 ", "4 ", "5 ", "6 ", "7 ", "8 ", "9 "  
-index1 db ?
-index2 db ?
-index3 db ?
-index4 db ?   
-count_bit_1 db ?   
-count_bit_2 db ?
+instructions db "MOV ", "ADD ", "SUB ", "MUL ", "DIV ", "INC ", "DEC ", "NOP ", "SAL ", "SHR ", "SHL ", "ROR ", "ROL ", "SAR ","IDIV ", "IMUL "  
+registers db "AX ", "AL ", "AH ", "BX ", "BL ", "BH ", "CX ", "CL ", "CH ", "DX ", "DL ", "DH ", "SI ", "DI ", "SP ", "BP ","val ","address "
+digits db "0 ", "1 ", "2 ", "3 ", "4 ", "5 ", "6 ", "7 ", "8 ", "9 ","A ","B ","C ","D ","E ","F "  
 
-;Forbidden Arrays to check if it is forbidden or not, if the corresponding index has value 1, it is forbidden  
-Forbidden_digits_1 db '0000000000'
+instruction_index db 0 
+; mov-> 0 add-> 1 sub-> 2
+; MUL-> 3 DIV-> 4 IDIV-> 14 IMUL-> 15  INC-> 5 DEC-> 6 
+; NOP-> 7
+; SAL-> 8 SHR-> 9 SHL-> 10 ROR-> 11 ROL-> 12 SAR->13
+
+src_index_reg db 0
+dest_index_reg db 17
+src_index_val db 1
+dest_index_val db 18 
+address_mode db 0 
+address_mode2 db 1  
+count_bit_1 db 1   
+count_bit_2 db 1
+ 
+losepoint db 0
+
+
+; player1 Data variables -----------------------------------------------------------------------------------
+Forbidden_digits_1 db '0000000000000000'
 Forbidden_Registers_1 db '0000000000000000'
-Forbidden_instruction_1 db '00000000000000000' 
-Forbidden_char1 db 'o'
-Forbidden_digits_2 db '0000000000'
-Forbidden_Registers_2 db '0000000000000000'
-Forbidden_instruction_2 db '00000000000000000'
-Forbidden_char2 db 'h'   
+Forbidden_instruction_1 db '0000000000000000' 
+Forbidden_char1 db 'B'
+intial_points_player1 db 60
+losepoint_player1 db 0
 
+data_segment_1 db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 
+Player1_Data_Register db 11h,22h,00h,00h,00h,00h,00h,00h,00h,00h,00h,00h,00h,00h,00h,00h,00h,00h,00h,05h
+;------------------------AH  -Al -BH-Bl -CH  -CL-DH  -DL-  SI -     DI --   SP    - BP   -value-addressvalue----
+
+; player2 Data variables --------------------------------------------------------------
+
+Forbidden_digits_2 db '0000000000000000'
+Forbidden_Registers_2 db '0000000000000000'
+Forbidden_instruction_2 db '0000000000000000'
+Forbidden_char2 db 'A'  
+intial_points_player2 db 60
+losepoint_player2 db 0
+
+data_segment_2 db 0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0
+Player2_Data_Register db 03h,02h,00h,07h,00h,00h,00h,00h,00h,00h,00h,00h,00h,00h,00h,00h,05h,05h,00h,05h
+;------------------------AH  -Al -BH-Bl -CH  -CL-DH  -DL-  SI -     DI --   SP    - BP   -value-addressvalue----   
  
 ;Comp_size to check if forbidden character is in array or not
 ; if 0 means its not there
@@ -34,7 +58,7 @@ check_forbidden macro Forbidden_array,index
     push bx
     mov bh,0
     mov bl,index  
-    mov ah,Forbidden_array+bx ;compare position at index in forbidden array
+    mov ah,Forbidden_array[bx] ;compare position at index in forbidden array
     cmp ah,'1' ; compare        
     pop bx  
     pop Ax
@@ -61,7 +85,7 @@ set_forbidden macro instruction_name,Forbidden_array,Forbidden_char,size,index
     Set_bit:
             mov bl,index
             mov bh,0
-            mov Forbidden_array+bx,'1' ; forbidden for instruction with index = index 
+            mov Forbidden_array[bx],'1' ; forbidden for instruction with index = index 
     Dont_Set_bit:
     pop bx  
     pop Cx
@@ -69,19 +93,11 @@ set_forbidden macro instruction_name,Forbidden_array,Forbidden_char,size,index
     pop Di
 Endm check_forbidden
 
-set_forbidden_digit macro 
-    push DI
-    push Ax
-    push Cx 
-    push bx 
-
-
-    endm set_forbidden_digit
 
 .code
            
            
-set_forbidden_proc proc near
+set_forbidden_player1 proc near
 ;--------------------------- assume we have it Read it Later in Forbidden_Char2-----------------------------------------;     
     
     ; loop on 17 instruction to set 
@@ -93,35 +109,22 @@ set_forbidden_proc proc near
         set_forbidden cx,Forbidden_instruction_2,Forbidden_char2,4,dl
         inc dl
         add si,4              
-        cmp si,24
+        cmp si,56
         jnz loop_set_instruction  
         
         lea cx ,instructions[24] 
         set_forbidden cx ,Forbidden_instruction_2,Forbidden_char2,3,6 
         
-        mov si,27
-        mov dl,7
+        mov si,56
+        mov dl,14
     loop_set_instruction2: 
         lea cx ,instructions[si]
-        set_forbidden cx,Forbidden_instruction_2,Forbidden_char2,4,dl
+        set_forbidden cx,Forbidden_instruction_2,Forbidden_char2,5,dl
         inc dl
-        add si,4              
-        cmp si,51
+        add si,5              
+        cmp si,66
         jnz loop_set_instruction2     
-        
-        lea cx ,instructions[51] 
-        set_forbidden cx ,Forbidden_instruction_2,Forbidden_char2,5,13 
-
-        mov si,56
-        
-        mov dl,14
-    loop_set_instruction3: 
-        lea cx ,instructions[si]
-        set_forbidden cx,Forbidden_instruction_2,Forbidden_char2,4,dl
-        inc dl
-        add si,4              
-        cmp si,68
-        jnz loop_set_instruction3    
+           
 ;------------- loop on numbers ----------------------------------------------      
     mov si,0
     mov dl,0 
@@ -130,7 +133,7 @@ set_forbidden_proc proc near
         set_forbidden cx,Forbidden_digits_2,Forbidden_char2,2,dl
         inc dl
         add si,2              
-        cmp si,10
+        cmp si,32
         jnz loop_set_numbers  
                                                                          
                                                                             
@@ -146,7 +149,61 @@ set_forbidden_proc proc near
         cmp si,48
         jnz loop_set_registers         
     ret
-    set_forbidden_proc endp           
+    set_forbidden_player1 endp
+
+set_forbidden_player2 proc near   ; player2 setforbidden for player1
+;--------------------------- assume we have it Read it Later in Forbidden_Char2-----------------------------------------;     
+    
+    ; loop on 17 instruction to set 
+;------instructoins,Forbidden_instruction,Forbidden_char2,17,------------------------------- 
+    mov si,0
+    mov dl,0 
+    loop_set2_instruction: 
+        lea cx ,instructions[si]
+        set_forbidden cx,Forbidden_instruction_1,Forbidden_char1,4,dl
+        inc dl
+        add si,4              
+        cmp si,56
+        jnz loop_set2_instruction  
+        
+        lea cx ,instructions[24] 
+        set_forbidden cx ,Forbidden_instruction_1,Forbidden_char1,3,6 
+        
+        mov si,56
+        mov dl,14
+    loop_set2_instruction2: 
+        lea cx ,instructions[si]
+        set_forbidden cx,Forbidden_instruction_1,Forbidden_char1,5,dl
+        inc dl
+        add si,5              
+        cmp si,66
+        jnz loop_set2_instruction2     
+           
+;------------- loop on numbers ----------------------------------------------      
+    mov si,0
+    mov dl,0 
+    loop_set2_numbers: 
+        lea cx ,digits[si]
+        set_forbidden cx,Forbidden_digits_1,Forbidden_char1,2,dl
+        inc dl
+        add si,2              
+        cmp si,32
+        jnz loop_set2_numbers  
+                                                                         
+                                                                            
+;------------- loop on regsiter 16------------------------------------------    
+
+    mov si,0
+    mov dl,0 
+    loop_set2_registers: 
+        lea cx ,registers[si]
+        set_forbidden cx,Forbidden_Registers_1,Forbidden_char1,3,dl
+        inc dl
+        add si,3              
+        cmp si,48
+        jnz loop_set2_registers         
+    ret
+    set_forbidden_player2 endp
 main proc far
     mov ax,@data
     mov ds,ax     
@@ -155,9 +212,9 @@ main proc far
     ; get forbidden character      
     
 ;--------------------------- assume we have it Read it Later in Forbidden_Char2-----------------------------------------;     
-       call set_forbidden_proc
+       call set_forbidden_player1
     
-    
+       call set_forbidden_player2
     
     
     
