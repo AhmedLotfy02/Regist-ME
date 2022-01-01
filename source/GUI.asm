2004cea4dbde1afb2b81f073aa93aea0174d323e
mohamednabilabdelfattah <76039904+mohamednabilabdelfattah@users.noreply.github.com>
2022-01-01 11:39:24 +0200

    .

--- source/GUI.asm
+++ source/GUI.asm
@@ -108,6 +108,57 @@ DISPLAY_2_DIGITS_NEW    MACRO   NUMBER
         POP AX
     ENDM
 
+print2Number MACRO num
+    local numerical,next,numerical2,next2
+    pusha
+    mov al,byte ptr num
+    mov ah,0
+    mov cl,10h
+    div cl
+    cmp al,0ah
+    jb numerical
+    add al,37h     
+    mov dl,  al 
+    push ax
+    mov ah,  2h
+    int 21h 
+    pop ax
+    jmp next
+    numerical:
+    add al,30h 
+   
+    mov dl,  al
+    push ax
+    mov ah,  2h
+    int 21h 
+    pop ax
+    next:
+    cmp ah,0ah
+    jb numerical2
+    add ah,37h     
+    mov dl,  ah 
+    push ax
+    mov ah,  2h
+    int 21h
+    pop ax 
+    jmp next2
+    numerical2:
+    add ah,30h 
+    mov dl,  ah
+    push ax
+    mov ah,  2h
+    int 21h
+    pop ax
+    next2:
+    
+    popa
+
+ENDM   
+
+print4number macro num
+    print2number num[1]
+    print2number num[0]
+endm
 
     drawFilledRec MACRO xb,yb,xen,yen,color
         local firstLoop,secondLoop  
